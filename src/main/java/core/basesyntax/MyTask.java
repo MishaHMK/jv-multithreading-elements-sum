4c1176b0576208b333852638f0f0f793099adda0
MishaHMK <gumeniukmich@gmail.com>
2025-06-06 20:42:15 +0300

    Task is done

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +16,35 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        int difference = finishPoint - startPoint;
+        if (difference > THRESHOLD) {
+            System.out.println("Splitting task, workLoad : " + difference);
+            List<RecursiveTask<Long>> subTasks = new ArrayList<>(createSubTasks());
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            long result = 0;
+            for (RecursiveTask<Long> subTask : subTasks) {
+                result += subTask.join();
+            }
+            return result;
+        }  else {
+            System.out.println("Doing task myself, workLoad: " + difference);
+            long sum = 0L;
+            for (long i = startPoint; i < finishPoint; i++) {
+                sum += i;
+            }
+            return sum;
+       }
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        int mid = startPoint + (finishPoint - startPoint) / 2;
+        RecursiveTask<Long> first = new MyTask(startPoint, mid);
+        RecursiveTask<Long> second = new MyTask(mid, finishPoint);
+        subTasks.add(first);
+        subTasks.add(second);
+        return subTasks;
     }
 }
